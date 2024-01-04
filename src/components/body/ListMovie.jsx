9cd6f9f9dcc9ba767a9bbf86eeee1d6ab2137d74
EduardoYpanaque0 <beduardo338@gmail.com>
2024-01-03 21:11:39 -0500

    avance 3-1

--- src/components/body/ListMovie.jsx
+++ src/components/body/ListMovie.jsx
@@ -1,7 +1,35 @@
 import React from 'react'
+// import Button from 'react-bootstrap/Button';
+// import Card from 'react-bootstrap/Card';
+import Image from 'react-bootstrap/Image';
+
 
 export default function ListMovie() {
   return (
-    <div>ListMovie</div>
+    <>
+    {/* <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card> */}
+    <div className='d-flex flex-column gap-1'>
+        <Image 
+            src="https://image.tmdb.org/t/p/original/6FfCtAuVAW8XJjZ7eWeLibRLWTw.jpg" 
+            className='ListImg'
+            thumbnail />
+        <h3>Titulo</h3>
+        <p >year</p>
+    </div>
+
+
+
+    
+    </>  
   )
 }
--- src/components/body/SelectedMovie.jsx
+++ src/components/body/SelectedMovie.jsx
@@ -1,29 +1,30 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import KeyboardDoubleArrowRightIcon from '@mui/icons-material/KeyboardDoubleArrowRight';
 
 export default function SelectedMovie() {
   return (
     // <div>SelectedMovie</div>
     // <img className='imgMovieSelected' src='https://m.media-amazon.com/images/S/pv-target-images/2d0c9e38968936e6711c7fb2bc7895b82d8bb9178b5a854e14dffa4b17b88487.jpg' />
+    <>
     <div
         className='viewDetail'
         style={{
             backgroundImage: `url('https://image.tmdb.org/t/p/original/4qCqAdHcNKeAHcK8tJ8wNJZa9cx.jpg')`,
         }}
     >
-        
-        <button
-            className="boton"
-            // onClick={() => setPlaying(true)}
-            type="button"
-        >
-            Ver Trailer
-        </button>
+        <Link 
+            to="/moviedetails"
+            className='moviedetails btnfos btnfos-5'
+        >Detalles <KeyboardDoubleArrowRightIcon className='Next'/>
+        </Link>
 
         <div className="contenido">
             <h1>Titulo</h1>
             <p>Descripción</p>
         </div>
     </div>
+    </>
     
   )
 }
