6163648038ba2fb45c787bc92e49f6fa74a4bef5
EduardoYpanaque0 <beduardo338@gmail.com>
2024-01-05 14:53:30 -0500

    avance 5

--- src/components/body/SelectedMovie.jsx
+++ src/components/body/SelectedMovie.jsx
@@ -1,3 +1,31 @@
+import React from "react";
+import "./SelectedMovie.css";
+import { Link } from "react-router-dom";
+import KeyboardDoubleArrowRightIcon from "@mui/icons-material/KeyboardDoubleArrowRight";
+
+export default function SelectedMovie() {
+  return (
+    <div className="selectedMovie">
+      <div
+        className="selectedMovie__details"
+        style={{
+          backgroundImage: `url('https://image.tmdb.org/t/p/original/4qCqAdHcNKeAHcK8tJ8wNJZa9cx.jpg')`,
+        }}
+      >
+        <Link to="/moviedetails" className="moviedetails">
+          <h2>Detalles</h2>
+          <KeyboardDoubleArrowRightIcon className="Next" />
+        </Link>
+
+        <div className="contenido">
+          <h1>Titulo Titulo Titulo Titulo Titulo Titulo Titulo  </h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+
 // import React from 'react'
 // import { Link } from 'react-router-dom'
 // import KeyboardDoubleArrowRightIcon from '@mui/icons-material/KeyboardDoubleArrowRight';
@@ -27,29 +55,3 @@
 //     </>
     
 //   )
-import React from "react";
-import "./SelectedMovie.css";
-import { Link } from "react-router-dom";
-import KeyboardDoubleArrowRightIcon from "@mui/icons-material/KeyboardDoubleArrowRight";
-
-export default function SelectedMovie() {
-  return (
-    <div className="selectedMovie">
-      <div
-        className="selectedMovie__details"
-        style={{
-          backgroundImage: `url('https://image.tmdb.org/t/p/original/4qCqAdHcNKeAHcK8tJ8wNJZa9cx.jpg')`,
-        }}
-      >
-        <Link to="/moviedetails" className="moviedetails btnfos btnfos-5">
-          <h2>Detalles</h2>
-          <KeyboardDoubleArrowRightIcon className="Next" />
-        </Link>
-
-        <div className="contenido">
-          <h1>Titulo Titulo Titulo Titulo Titulo Titulo Titulo Titulo </h1>
-        </div>
-      </div>
-    </div>
-  );
-}
