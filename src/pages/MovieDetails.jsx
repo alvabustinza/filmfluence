98b42823a1c90c5d071a04dcb084d60246847826
alvabustinza <85914595+alvabustinza@users.noreply.github.com>
2024-01-05 18:49:24 -0500

    merge eduardo-master

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import "./MovieDetails.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import StarBorderIcon from "@mui/icons-material/StarBorder";
 import StarIcon from "@mui/icons-material/Star";
 import PlayCircleIcon from "@mui/icons-material/PlayCircle";
 import VideoFileIcon from "@mui/icons-material/VideoFile";
 import { Image, FloatingLabel, Form, Button, Modal } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import YouTube from "react-youtube";
 import { database } from "../firebase";
 import { ref, set } from "firebase/database";
@@ -30,7 +30,7 @@ function MyVerticallyCenteredModal(props) {
       </Modal.Header>
       <Modal.Body>
         <YouTube
-          videoId={22}
+          videoId={props.id}
           className="reproductor container"
           containerClassName={"youtube-container amru"}
           opts={{
@@ -48,9 +48,6 @@ function MyVerticallyCenteredModal(props) {
             },
           }}
         />
-        <button onClick={() => setPlaying(false)} className="boton">
-          Close
-        </button>
       </Modal.Body>
       <Modal.Footer>
         <Button onClick={props.onHide}>Close</Button>
@@ -60,12 +57,31 @@ function MyVerticallyCenteredModal(props) {
 }
 
 export default function MovieDetails() {
+  let { state } = useLocation();
+  const urlFirst = "https://image.tmdb.org/t/p/original";
+
   const [textareaValue, setTextareaValue] = useState("");
   const [rating, setRating] = useState(null);
+  // const [ident, setIdent] = useState(null)
+  // setIdent(state.movie.id);
+  const ident = state.movie.id;
+  const movie = state.movie;
+
+  const [trailer, setTrailer] = useState(null);
+  const [playing, setPlaying] = useState(false);
+
+  if (state.movie.videos && state.movie.videos.results) {
+    const trailer = state.movie.videos.results.find(
+      (vid) => vid.name === "Official Trailer"
+    );
+    setTrailer(trailer ? trailer : state.movie.videos.results[0]);
+  }
+  //return data
+
   const [hover, setHover] = useState(null);
   const [modalShow, setModalShow] = React.useState(false);
 
-  const dateObject = new Date("1977-05-25");
+  const dateObject = new Date(state.movie.release_date);
   const day = dateObject.getDate();
   const month = dateObject.toLocaleString("default", { month: "long" }); // Obtiene el nombre del mes
   const year = dateObject.getFullYear();
@@ -85,6 +101,18 @@ export default function MovieDetails() {
         console.error("Error al guardar datos:", error);
       });
   };
+  const saveToDB = () => {
+    const data = {
+      comentarios: textareaValue,
+      clasificacion: rating,
+      identificador: ident,
+    };
+
+    const jsonData = JSON.stringify(data);
+    saveData(jsonData);
+
+    // Lógica adicional para guardar en la base de datos si es necesario
+  };
 
   return (
     <>
@@ -93,26 +121,26 @@ export default function MovieDetails() {
         className="d-flex justify-content-center align-items-center align-items-md-start m-5 gap-4 flex-column flex-md-row"
       >
         <Image
-          src="https://image.tmdb.org/t/p/original/6FfCtAuVAW8XJjZ7eWeLibRLWTw.jpg"
+          src={urlFirst + state.movie.poster_path} //"https://image.tmdb.org/t/p/original/6FfCtAuVAW8XJjZ7eWeLibRLWTw.jpg"
           className="ListImg p-2"
           thumbnail
         />
         <div className="d-flex flex-column confWidth">
           <h1 className="m-0">
-            Star Wars Collection{" "}
+            {state.movie.title}
             <span className="YearDetails">
-              ({day} de {month} de {year})
+              ( {day} de {month} de {year})
             </span>
           </h1>
           <div className="d-flex mt-1">
             <Link to="/" className="Genres">
-              Adventure{" "}
+              Adventure -
             </Link>
             <Link to="/" className="Genres">
-              Action{" "}
+              Action -
             </Link>
             <Link to="/" className="Genres">
-              Science Fiction{" "}
+              Science Fiction
             </Link>
           </div>
           {/* <Link 
@@ -120,11 +148,7 @@ export default function MovieDetails() {
                     className='moviedetails btnfos btnfos-5'
                 >Detalles <KeyboardDoubleArrowRightIcon className='Next'/>
                 </Link> */}
-          <p className="m-0 ms-1 mt-3">
-            Descripcion de la peliculaDescripcion de la peliculaDescripcion de
-            la peliculaDescripcion de la peliculaDescripcion de la
-            peliculaDescripcion de la peliculaDescripcion de la pelicula
-          </p>
+          <p className="m-0 ms-1 mt-3">{state.movie.overview}</p>
           <FloatingLabel
             controlId="floatingTextarea2"
             label="Comentarios"
@@ -139,6 +163,7 @@ export default function MovieDetails() {
               }}
               value={textareaValue}
               onChange={handleTextareaChange}
+              required
             />
           </FloatingLabel>
           <div className="d-flex align-items-start mt-3">
@@ -151,6 +176,7 @@ export default function MovieDetails() {
                     name="rating"
                     value={currentRating}
                     onClick={() => setRating(currentRating)}
+                    required
                   />
                   {/* <StarBorderIcon 
                                 className='star' 
@@ -187,7 +213,7 @@ export default function MovieDetails() {
             <Button
               className="confWidhtButton"
               variant="primary"
-              onClick={saveData("Hola")}
+              onClick={saveToDB}
             >
               Guardar <VideoFileIcon />
             </Button>
